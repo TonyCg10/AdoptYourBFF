77b807d795ea346eb4ccdbc626c7c094d0c21621
TonyCg10 <103067004+TonyCg10@users.noreply.github.com>
2024-12-14 16:38:34 -0500

    1

---
+++ AdoptYourBFF/src/App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
